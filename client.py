0d6eb975aa562c5ce72fc80c921744014efcd147
7senx <89274663+7senx@users.noreply.github.com>
2024-05-18 17:51:05 +0300

    Added search headlines function

--- client.py
+++ client.py
@@ -8,8 +8,8 @@ def receive(cs):
     response = cs.recv(4096).decode("utf-8")
     return json.loads(response)
 
-def send(option,cs):
-    cs.send(option.encode("utf-8"))
+def send(item,cs):
+    cs.send(item.encode("utf-8"))
 
 def start_client():
     cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,6 +37,40 @@ def start_client():
         else:
             print("Invalid option")
     cs.close    
+
+def search_headline(option,cs):
+
+    print("Select option: ")
+    print("1- Search for Keywords")
+    print("2- Search by Category")
+    print("3- Search by Country")
+    print("4- List all New Headlines")
+    print("5- Back to Main Menu")
+
+    if option == '5':
+        return
+    elif option in ['1','2','3','4']:
+        response = receive(cs)
+        print("Results: ")
+
+        for index, item in enumerate(response['articles']):
+            print(f"{index + 1}. {item['title']} - {item['description']}")
+
+        selected_item = int(input("Enter article number: "))
+        
+        send(str(selected_item), client_socket)
+        response = receive(client_socket)
+        print("Article Details:")
+        print(f"Source: {response['source']['name']}")
+        print(f"Author: {response['author']}")
+        print(f"Title: {response['title']}")
+        print(f"URL: {response['url']}")
+        print(f"Description: {response['description']}")
+        print(f"Published At: {response['publishedAt']}")
+        print(f"Content: {response['content']}")
+
+
+
 
 
 
