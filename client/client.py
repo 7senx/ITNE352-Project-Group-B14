1fdc5bdf0e11a89ee885a8c4fe1d0a71ef274bda
Husain <202108591@stu.uob.edu.bh>
2024-05-26 22:32:56 +0300

    small improvements

--- client/client.py
+++ client/client.py
@@ -9,8 +9,8 @@ LANGUAGES = ["ar", "en"]
 def start_client():
     #init client socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #connect to server
     client_socket.connect((SERVER_IP, SERVER_PORT))
-
     print(f"Connected to server {SERVER_IP}:{SERVER_PORT}")
     client_name = input("Enter your name: ")
     send(client_name, client_socket)
@@ -23,9 +23,9 @@ def start_client():
 
         option = int(input("Enter your option: "))
         if option == 1:
-            headline(client_socket)
+            getHeadline(client_socket)
         elif option == 2:
-            source(client_socket)
+            getSource(client_socket)
         elif option == 3:
             print("Quitting program...")
             client_socket.close()
@@ -34,7 +34,7 @@ def start_client():
             print("Invalid option. Please try again.")
     
 
-def headline(client_socket):
+def getHeadline(client_socket):
     print("Select option: ")
     print("1- Search for Keywords")
     print("2- Search by Category")
@@ -82,7 +82,7 @@ def headline(client_socket):
         print("Invalid option")
         return
     
-def source(client_socket):
+def getSource(client_socket):
     print("Select option: ")
     print("1- Search by category")
     print("2- Search by country")
@@ -123,7 +123,7 @@ def source(client_socket):
 
     #list all
     elif sub_option == 4:
-        msg = f"2.{sub_option}:x"
+        msg = f"2.{sub_option}:null"
         send(msg, client_socket)
         receive_sources(client_socket)
 
@@ -137,15 +137,17 @@ def source(client_socket):
 
 def receive_headlines( client_socket):
     print("Receiving Headlines")
+    #receiving headlines data
     headlines_data = client_socket.recv(100000).decode()
     headlines_list = json.loads(headlines_data)
+    #printing headlines list
     print("Results :")
     for index, item in enumerate(headlines_list['articles']):
         print(f"{index + 1}. {item['title']} - {item['description']}")
-
+    #take input from user to select the article he wants to read
     selected = int(input("Enter the article number: "))
     selected_headline = headlines_list['articles'][selected-1]
-
+    #printing selected article details
     print(f"Article {selected} Details:")
     print(f"Source: {selected_headline['source']['name']}")
     print(f"Author: {selected_headline['author']}")
@@ -155,17 +157,18 @@ def receive_headlines( client_socket):
     print(f"Published At: {selected_headline['publishedAt']}")
 
 def receive_sources( client_socket):
-
     print("Receiving Sources")
+    #receiving sources data
     sources_data = client_socket.recv(100000).decode()
     sources_list = json.loads(sources_data)
+    #printing sources list
     print("Results for:")
     for index, item in enumerate(sources_list['sources']):
         print(f"{index + 1}. {item['name']}")
-
+    #take input from user to select the source he wants to read
     selected = int(input("Enter the source number: "))
     selected_source = sources_list['sources'][selected-1]
-
+    #printing selected source details
     print(f" Source {selected} Details:")
     print(f"Source: {selected_source['name']}")
     print(f"Country: {selected_source['country']}")
@@ -174,6 +177,7 @@ def receive_sources( client_socket):
     print(f"Category: {selected_source['category']}")
     print(f"Language: {selected_source['language']}")
 
+# a function to send messages to the server
 def send(msg, client_socket):
     client_socket.send(msg.encode())
 
